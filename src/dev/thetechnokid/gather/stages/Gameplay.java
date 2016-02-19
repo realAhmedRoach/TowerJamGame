569e91e109bed13d499bdcffbb524f47e4bcdf3a
theTechnoKid <thetechnokid11@gmail.com>
2016-02-18 19:55:10 -0500

    More quick fixes

--- src/dev/thetechnokid/gather/stages/Gameplay.java
+++ src/dev/thetechnokid/gather/stages/Gameplay.java
@@ -11,6 +11,7 @@ public class Gameplay extends Stage {
 
 	private Countdown c;
 	private Enemy crap = new Enemy(false);
+
 	public Gameplay() {
 		createEntities();
 		Game.getCurrentGame().getScreen().getController().addEntity(crap);
@@ -19,40 +20,37 @@ public class Gameplay extends Stage {
 	}
 
 	private void createEntities() {
-		for (int y = 0; y < Game.getCurrentGame().getScreen().height
-				/ Tile.DRAW_SIZE; y++) {
-			for (int x = 0; x < Game.getCurrentGame().getScreen().width
-					/ Tile.DRAW_SIZE; x++) {
-				if (Math.random() < .3 && Math.random()<.3) {
+		for (int y = 0; y < Game.getCurrentGame().getScreen().height / Tile.DRAW_SIZE; y++) {
+			for (int x = 0; x < Game.getCurrentGame().getScreen().width / Tile.DRAW_SIZE; x++) {
+				if (Math.random() < .3 && Math.random() < .3) {
 					Ore c = new Ore();
 					c.x = x * Tile.DRAW_SIZE;
 					c.y = y * Tile.DRAW_SIZE;
-					Game.getCurrentGame().getScreen().getController()
-							.addEntity(c);
+					Game.getCurrentGame().getScreen().getController().addEntity(c);
 				}
 			}
 		}
-		
-		if(Game.getCurrentGame().getScreen().getController().getEntities().contains(Game.getCurrentGame().getScreen().getController().getUser())) {
-			
-			Game.getCurrentGame().getScreen().getController().addEntity(Game.getCurrentGame().getScreen().getController().getUser());
+
+		if (!Game.getCurrentGame().getScreen().getController().getEntities()
+				.contains(Game.getCurrentGame().getScreen().getController().getUser())) {
+			Game.getCurrentGame().getScreen().getController()
+					.addEntity(Game.getCurrentGame().getScreen().getController().getUser());
 		}
 	}
 
 	@Override
 	public void render(Graphics g, Screen screen) {
-		Text.render(Integer.toString(c.timeLeft()), g, 11*Tile.DRAW_SIZE, 0);
+		Text.render(Integer.toString(c.timeLeft()), g, 11 * Tile.DRAW_SIZE, 0);
 	}
 
 	@Override
 	public void tick() {
-		if(c.timeLeft()<=0) {
+		if (c.timeLeft() <= 0) {
 			Stage.setCurrentStage(new TowerStage());
 			System.out.println("done");
 		}
 		boolean done = false;
-		for (Entity entity : Game.getCurrentGame().getScreen().getController()
-				.getEntities()) {
+		for (Entity entity : Game.getCurrentGame().getScreen().getController().getEntities()) {
 			if (!(entity instanceof Creature)) {
 				Ore o = (Ore) entity;
 				if (!o.isMined()) {
@@ -62,12 +60,11 @@ public class Gameplay extends Stage {
 					done = true;
 				}
 			}
-		} if (done) {
-			for (Entity e : Game.getCurrentGame().getScreen().getController()
-					.getEntities()) {
-				if(!(e instanceof Player))
-					Game.getCurrentGame().getScreen().getController().getEntities()
-						.remove(e);
+		}
+		if (done) {
+			for (Entity e : Game.getCurrentGame().getScreen().getController().getEntities()) {
+				if (!(e instanceof Player))
+					Game.getCurrentGame().getScreen().getController().getEntities().remove(e);
 			}
 			createEntities();
 		}
